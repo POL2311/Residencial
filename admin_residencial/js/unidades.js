3874f5d660a273fcd5f22a5b610d13bf554a88c0
alatorre <alatorrek@hitss.com>
2026-04-08 11:07:20 -0600

    nuevo

--- admin_residencial/js/unidades.js
+++ admin_residencial/js/unidades.js
@@ -88,7 +88,7 @@ console.log('[UNIDADES] JS ACTIVO');
 
     try {
       const json = await fetchJSON(
-        '/Residencial/admin_residencial/php/api/unidades.php'
+        '/admin_residencial/php/api/unidades.php'
       );
 
       state.unidades = json.unidades || [];
@@ -242,7 +242,7 @@ console.log('[UNIDADES] JS ACTIVO');
 
     try {
       await fetchJSON(
-        '/Residencial/admin_residencial/php/api/unidades.php',
+        '/admin_residencial/php/api/unidades.php',
         { method: 'POST', body: fd }
       );
       loadUnidades();
